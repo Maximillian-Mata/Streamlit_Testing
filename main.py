51f4f1ab424e59507394ad5349d068c053092703
Maximillian Mata <maximillian.mata@unlv.edu>
2024-09-26 23:19:45 -0700

    let video download

--- main.py
+++ main.py
@@ -19,14 +19,22 @@ def GetVideo(url):
         yt = YouTube(url=url, use_po_token=True, token_file="Mytokens.txt")
         best = yt.streams.get_highest_resolution()
         best.download()
+        global Completed
+        Completed = True
+        global video_bytes
+        try:
+            with open(yt.title+".mp4", "rb") as file:
+                video_bytes = file.read()  # Read the binary content of the file
+        except FileNotFoundError:
+            st.error("The specified file was not found.")
+            video_bytes = None
         st.write("Download complete")
     except Exception as e:
         st.write(f"Error Occurred:{e}")
 
 
 
-
-
+Completed = False
 
 st.button("Get IP", on_click=GetIP)
 
@@ -44,6 +52,9 @@ with st.form("Youtube Download"):
     if submitted:
         GetVideo(url)
 
+
+if Completed:
+    st.download_button(data=video_bytes, label="Download Video", mime="video/mp4")
 
 # JS test
 
