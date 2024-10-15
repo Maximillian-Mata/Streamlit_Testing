a74c1e930f59a253c93d73b7aacef636bc2ac16c
Maximillian Mata <maximillian.mata@unlv.edu>
2024-10-15 13:12:16 -0700

    check values

--- main.py
+++ main.py
@@ -72,8 +72,10 @@ def GetVideo(url):
 st.button("Get IP", on_click=GetIP)
 get_token = st.button("Update Po")
 if get_token:
-    output = subprocess.run(['node', "potoken.js"], capture_output=True, text=True)
-    st.write(str(output))
+    subprocess.run(['node', "potoken.js"], capture_output=True, text=True)
+    f= open("Mytokens.txt")
+    st.write_stream(f.read())
+    f.close()
 
 
 st.header("Testing pytubefix")
